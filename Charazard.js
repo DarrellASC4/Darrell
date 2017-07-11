e8bc8f77b2e3ace928deb99469b3669fc6a15370
DarrellASC4 <ddraughnjr@gmail.com>
2017-07-11 09:00:25 -0700

    first commit

---
+++ Charazard.js
@@ -0,0 +1,7 @@
+var charizardnHP=78;
+var charizardNum=006;
+var charizardType="fire";
+var charizarBestMove="flame";
+var charizardLastEvo= false;
+var CarizardCatchRate=45;
+
---
+++ pokemon.js
@@ -0,0 +1,18 @@
+ myPokemonRoster = ["charizard","Dialga", "Mew","meow","Ditto", "Gsrdivor"];
+for(i=0; i<6; i++){
+    console.log(myPokemonRoster[i]);
+}; 
+//var FirstToFight = myPokemonRoster[0];load
+//var SecondToFight = myPokemonRoster[4];
+
+/*
+for(var i=0; i<10; i++){
+        console.log(i);
+}
+
+
+var myArray=["bob","Mahdi","cyril", "Juan", "mike"] 
+for(i=0; i<5; i++){
+    console.log(myArray[i]);
+}; 
+*/
